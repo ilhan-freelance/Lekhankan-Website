79dd6726f47c3e635892b54ee7b252fe927c7aa0
Mohammad-ilhan <mohammadilhan345@gmail.com>
2026-08-27 12:49:16 +0530

    Remove all sections after CONTACT US section except footer

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -20,12 +20,6 @@ require APPPATH . 'Views/sections/process.php';
 require APPPATH . 'Views/sections/about_lekhankan.php';
 require APPPATH . 'Views/sections/team.php';
 require APPPATH . 'Views/sections/contact.php';
-require APPPATH . 'Views/sections/brand_story.php';
-require APPPATH . 'Views/sections/brand_video.php';
-require APPPATH . 'Views/sections/virtual_dept.php';
-require APPPATH . 'Views/sections/lead_magnets.php';
-require APPPATH . 'Views/sections/insights.php';
-require APPPATH . 'Views/sections/final_cta.php';
 
 // Render Footer Template
 require APPPATH . 'Views/templates/footer.php';
