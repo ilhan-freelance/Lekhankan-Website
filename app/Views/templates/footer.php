664bf8352c2349bb7649af92c90739c991f4ef32
Mohammad-ilhan <mohammadilhan345@gmail.com>
2026-09-03 17:53:36 +0530

    Refine top wave curve into a dramatic, high-crest flowing liquid wave (140px height)

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,8 +1,8 @@
 <!-- ═══ FOOTER — Light Organic Wave Curve & 4-Column Layout ═══ -->
 <footer class="lk-footer">
   <div class="footer-wave-wrap">
-    <svg class="footer-wave-svg" viewBox="0 0 1440 100" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-      <path d="M0,35 C320,90 680,85 1020,40 C1240,10 1360,30 1440,38 L1440,100 L0,100 Z" fill="#EFE8DB"></path>
+        <svg class="footer-wave-svg" viewBox="0 0 1440 140" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+      <path d="M 0, 30 C 260, -20 540, 20 800, 75 C 1040, 120 1260, 70 1440, 55 L 1440, 140 L 0, 140 Z" fill="#EFE8DB"></path>
     </svg>
   </div>
 
